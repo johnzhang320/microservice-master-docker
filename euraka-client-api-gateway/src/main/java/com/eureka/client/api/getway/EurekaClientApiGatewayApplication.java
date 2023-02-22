c2468b4df4748d4ce324fb42a6564eedd8f13a97
johnzhang320 <johnzhang320@gmail.com>
2023-02-22 14:02:40 -0800

    upgrade to JDK 17

--- euraka-client-api-gateway/src/main/java/com/eureka/client/api/getway/EurekaClientApiGatewayApplication.java
+++ euraka-client-api-gateway/src/main/java/com/eureka/client/api/getway/EurekaClientApiGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaClientApiGetwayApplication {
+public class EurekaClientApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApiGetwayApplication.class, args);
+		SpringApplication.run(EurekaClientApiGatewayApplication.class, args);
 	}
 
 }
